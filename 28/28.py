95f568d087c3cbfd2a643ea068a9c523e19a5288
yukienomiya <yn.yuki98@gmail.com>
2020-02-22 10:57:06 +0100

    Add decription file

--- 28/28.py
+++ 28/28.py
@@ -1,22 +1,3 @@
-""" 
-PROBLEM 28
-Given an integer n, find the sum of the numbers placed on the two diagonals of the matrix (or spiral) 
-n*n. The matrix/spiral is formed starting with the number 1 and moving to the right in a clockwise 
-direction.
-
-example:
-n = 5
-21 22 23 24 25
-20  7  8  9 10
-19  6  1  2 11
-18  5  4  3 12
-17 16 15 14 13
-
-The output should be the sum of 25, 21, 17, 13, 9, 7, 5, 3, 1. The answer is therefore 101. 
-"""
-
-
-
 def es(num):
   lastNum = num * num
   somma = lastNum
